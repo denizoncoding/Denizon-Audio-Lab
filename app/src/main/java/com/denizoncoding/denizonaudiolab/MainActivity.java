e01ac53f15d975cadf611ccab9aef413de4813f3
L700-deniz <dnzatlhn91@gmail.com>
2019-03-26 00:02:31 +0300

    ui start

--- app/src/main/java/com/denizoncoding/denizonaudiolab/MainActivity.java
+++ app/src/main/java/com/denizoncoding/denizonaudiolab/MainActivity.java
@@ -2,28 +2,86 @@ package com.denizoncoding.denizonaudiolab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    private ToggleButton onOffbutton;
+
+    private RadioButton radioButtonSine;
+    private RadioButton radioButtonSquare;
+    private RadioButton radioButtonSawtooth;
+    private RadioButton radioButtonTriangular;
+
+    private SeekBar seekBarFrequency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        createUIElements();
+
+        setUIListeners();
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void createUIElements() {
+
+        onOffbutton = findViewById(R.id.toggleButton);
+
+        radioButtonSine = findViewById(R.id.radioButtonSine);
+        radioButtonSquare = findViewById(R.id.radioButtonSquare);
+        radioButtonSawtooth = findViewById(R.id.radioButtonSawtooth);
+        radioButtonTriangular = findViewById(R.id.radioButtonTriangular);
+
+        seekBarFrequency = findViewById(R.id.seekBarFrequency);
+
+    }
+
+    private void setUIListeners() {
+
+        onOffbutton.setOnClickListener(this);
+
+        radioButtonSine.setOnCheckedChangeListener(this);
+        radioButtonSine.setOnCheckedChangeListener(this);
+        radioButtonSine.setOnCheckedChangeListener(this);
+        radioButtonSine.setOnCheckedChangeListener(this);
+
+        seekBarFrequency.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
 }
