7008d0d8008e4646eea769bf3a87ed04538a01c6
denizoncoding <dnzatlhn91@gmail.com>
2019-03-27 00:15:25 +0300

    ui fix

--- app/src/main/java/com/denizoncoding/denizonaudiolab/MainActivity.java
+++ app/src/main/java/com/denizoncoding/denizonaudiolab/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
-import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener {
@@ -33,18 +33,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setUIListeners();
 
+        radioButtonSine.setChecked(true);
     }
 
     private void createUIElements() {
 
-        onOffbutton = findViewById(R.id.toggleButton);
+        this.onOffbutton = findViewById(R.id.toggleOnOffButton);
 
-        radioButtonSine = findViewById(R.id.radioButtonSine);
-        radioButtonSquare = findViewById(R.id.radioButtonSquare);
-        radioButtonSawtooth = findViewById(R.id.radioButtonSawtooth);
-        radioButtonTriangular = findViewById(R.id.radioButtonTriangular);
+        this.radioButtonSine = findViewById(R.id.radioButtonSine);
+        this.radioButtonSquare = findViewById(R.id.radioButtonSquare);
+        this.radioButtonSawtooth = findViewById(R.id.radioButtonSawtooth);
+        this.radioButtonTriangular = findViewById(R.id.radioButtonTriangular);
 
-        seekBarFrequency = findViewById(R.id.seekBarFrequency);
+        this.seekBarFrequency = findViewById(R.id.seekBarFrequency);
 
     }
 
@@ -53,9 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         onOffbutton.setOnClickListener(this);
 
         radioButtonSine.setOnCheckedChangeListener(this);
-        radioButtonSine.setOnCheckedChangeListener(this);
-        radioButtonSine.setOnCheckedChangeListener(this);
-        radioButtonSine.setOnCheckedChangeListener(this);
+        radioButtonSquare.setOnCheckedChangeListener(this);
+        radioButtonSawtooth.setOnCheckedChangeListener(this);
+        radioButtonTriangular.setOnCheckedChangeListener(this);
 
         seekBarFrequency.setOnSeekBarChangeListener(this);
     }
@@ -63,16 +64,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()) {
+            case R.id.toggleOnOffButton:
+                if (onOffbutton.isChecked()) {
+
+                    Toast.makeText(this, "On", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(this, "Off", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
+        if (radioButtonSine.isChecked()) {
+
+            Toast.makeText(this, "Sine", Toast.LENGTH_SHORT).show();
+        } else if (radioButtonSquare.isChecked()) {
+
+            Toast.makeText(this, "Square", Toast.LENGTH_SHORT).show();
+        } else if (radioButtonSawtooth.isChecked()) {
+
+            Toast.makeText(this, "Sawtooth", Toast.LENGTH_SHORT).show();
+        } else if (radioButtonTriangular.isChecked()) {
+
+            Toast.makeText(this, "Triangular", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
+        Toast.makeText(this, "" + seekBar.getProgress(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
